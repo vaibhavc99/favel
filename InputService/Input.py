cc15928e80697741d734038de10b2ea87e023ae2
Sascha Trippel <strippel@mail.uni-paderborn.de>
2022-05-27 10:49:06 +0200

    Implemented rudimentary input reader

---
+++ InputService/Input.py
@@ -0,0 +1,15 @@
+class Input:
+    # TODO: this is just an example method
+    
+    def getInput(self, filePath:str):
+        result = []
+        with open(filePath, 'r') as file:
+            for line in file.readlines():
+                result.append(self.parseLine(line))
+        return result 
+                
+    def parseLine(self, line:str):
+        line = line.replace('\n', '')
+        line = line.replace('"', '')
+        elements = line.split(' ')
+        return (elements[0], elements[1], elements[2].replace('.', ''))
