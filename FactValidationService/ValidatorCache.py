9754f52fa000d582f8904238f0acc7e00484e63b
Sascha Trippel <strippel@mail.uni-paderborn.de>
2022-05-27 14:13:28 +0200

    Implemented cache for assertions

---
+++ FactValidationService/ValidatorCache.py
@@ -0,0 +1,29 @@
+import sqlite3, logging
+
+class ValidatorCache:
+    def __init__(self, dbpath:str):
+        self.db = sqlite3.connect(dbpath)
+        try:
+            self.createTable()
+        except sqlite3.OperationalError as ex:
+            pass
+        
+    def createTable(self):
+        self.db.execute('''CREATE TABLE validatorCache
+        (approach TEXT NOT NULL,
+        subject TEXT NOT NULL,
+        predicate TEXT NOT NULL,
+        object TEXT NOT NULL,
+        score REAL NOT NULL);''')
+        
+    def close(self):
+        self.db.close()
+        
+    def insert(self, approach:str, sub:str, pred:str, obj:str, score:float):
+        self.db.execute("INSERT INTO validatorCache (approach, subject, predicate, object, score) VALUES ('{}', '{}', '{}', '{}', {})".format(approach, sub, pred, obj, score))
+        self.db.commit()
+    
+    def getScore(self, approach:str, sub:str, pred:str, obj:str):
+        cursor = self.db.execute('SELECT score FROM validatorCache WHERE approach="{}" AND subject="{}" AND predicate="{}" AND object="{}"'.format(approach, sub, pred, obj))
+        for row in cursor:
+            return row[0]
