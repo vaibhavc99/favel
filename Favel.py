279a3ba799aeb68ccc09cc92191e2bc1916b4dc2
Sascha Trippel <strippel@mail.uni-paderborn.de>
2022-06-01 15:33:09 +0200

    Cleanup

--- Favel.py
+++ Favel.py
@@ -5,23 +5,13 @@ from InputService.Input import Input
 
 def main():
     # Parse arguments
-    argumentParser = argparse.ArgumentParser()
-    argumentParser.add_argument("-d", "--data", required=True, help="Path to input data")
-    args = argumentParser.parse_args()
+    args = parseArguments()
     
     # Read config
-    configParser = configparser.ConfigParser()
-    configParser.read("favel.conf")
+    configParser = loadConfig()
 
     # Configure logging
-    loggingOptions = dict()
-    loggingOptions['debug'] = logging.DEBUG
-    loggingOptions['info'] = logging.INFO
-    loggingOptions['error'] = logging.ERROR
-    loggingOptions['critical'] = logging.CRITICAL
-    
-    logging.basicConfig(level=loggingOptions[configParser['General']['logging']])
-    
+    configureLoggin(configParser)
     
     # Read input
     input = Input()
@@ -34,6 +24,25 @@ def main():
     
     # Print results
     print(result)
+    
+def parseArguments():
+    argumentParser = argparse.ArgumentParser()
+    argumentParser.add_argument("-d", "--data", required=True, help="Path to input data")
+    return argumentParser.parse_args()
+    
+def loadConfig():
+    configParser = configparser.ConfigParser()
+    configParser.read("favel.conf")
+    return configParser
+
+def configureLoggin(configParser:configparser.ConfigParser):
+    loggingOptions = dict()
+    loggingOptions['debug'] = logging.DEBUG
+    loggingOptions['info'] = logging.INFO
+    loggingOptions['error'] = logging.ERROR
+    loggingOptions['critical'] = logging.CRITICAL
+    
+    logging.basicConfig(level=loggingOptions[configParser['General']['logging']])
     
 
 if __name__ == '__main__':
